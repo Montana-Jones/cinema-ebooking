fd90fd7beea8fe3efded490735e4ecfc1c3ba521
Montana-Jones <mgj58820@uga.edu>
2025-09-23 15:08:24 -0400

    created SearchBar
    created non-functional SearchBar

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -7,18 +7,22 @@ import Movie from "@/components/Movie";
 import MoviePreview from "@/components/MoviePreview";
 import dummyMovies from "@/data/DummyMovies";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main style={{ padding: "2rem" }}>
-      <div className={styles.movieScroll}>
+    <main>
+      <Navbar />
+      <div style={{ padding: "2rem" }}>
+        <div className={styles.movieScroll}>
+          {dummyMovies.map((movie, index) => (
+            <MoviePreview key={index} movie={movie} />
+          ))}
+        </div>
         {dummyMovies.map((movie, index) => (
-          <MoviePreview key={index} movie={movie} />
+          <Movie key={index} movie={movie} />
         ))}
       </div>
-      {dummyMovies.map((movie, index) => (
-        <Movie key={index} movie={movie} />
-      ))}
     </main>
   );
 }
--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -1,3 +1,37 @@
 "use client";
 
-export default function Navbar() {}
+import React from "react";
+import Image from "next/image";
+import avatar from "@/assets/avatar.png";
+import theatre from "@/assets/theatre.png";
+import Link from "next/link";
+import styles from "@/components/Card.module.css";
+import SearchBar from "@/components/SearchBar";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbarContainer}>
+      <div>
+        <Link href="/">
+          <Image
+            src={theatre}
+            alt="logo"
+            className={styles.navbarLogo}
+            priority
+          />
+        </Link>
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+      <div>
+        <Image
+          src={avatar}
+          alt="profile"
+          className={styles.navbarProfile}
+          priority
+        />
+      </div>
+    </nav>
+  );
+}
