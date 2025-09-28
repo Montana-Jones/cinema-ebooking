8ff63bb857292817234e4607bddde7610a7a4a63
uynguyen <189261130+un22952@users.noreply.github.com>
2025-09-28 01:04:39 -0400

    updated movieController

--- project/src/main/java/com/example/cinema_backend/controller/MovieController.java
+++ project/src/main/java/com/example/cinema_backend/controller/MovieController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cinema_backend.model.Movie;
@@ -36,27 +35,5 @@ public class MovieController {
         return ResponseEntity.ok(movieService.getMovieById(id));
     }
 
-    // GET /api/v1/movies/now-showing
-    @GetMapping("/now-showing")
-    public ResponseEntity<List<Movie>> getNowShowingMovies() {
-        return ResponseEntity.ok(movieService.getNowShowingMovies());
-    }
-
-    // GET /api/v1/movies/coming-soon
-    @GetMapping("/coming-soon")
-    public ResponseEntity<List<Movie>> getComingSoonMovies() {
-        return ResponseEntity.ok(movieService.getComingSoonMovies());
-    }
-
-    // GET /api/v1/movies/search?title=some_title
-    @GetMapping("/search")
-    public ResponseEntity<List<Movie>> searchMoviesByTitle(@RequestParam String title) {
-        return ResponseEntity.ok(movieService.searchMoviesByTitle(title));
-    }
-
-    // GET /api/v1/movies/filter?genre=some_genre
-    @GetMapping("/filter")
-    public ResponseEntity<List<Movie>> filterMoviesByGenre(@RequestParam String genre) {
-        return ResponseEntity.ok(movieService.filterMoviesByGenre(genre));
-    }
+    
 }
