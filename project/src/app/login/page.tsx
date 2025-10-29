435107a5a91996e8f746d9ca2321c7bc6ed304b6
uynguyen <189261130+un22952@users.noreply.github.com>
2025-10-28 23:01:51 -0400

    need to change config and add admin func

--- project/src/app/login/page.tsx
+++ project/src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
-import { useUser } from "@/app/context/UserContext";
+import { useUser } from "@/context/UserContext";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
